5ac404a2c9ea6030b3b12d4ee26e4c601bd1bfc0
Sam Beckmann <samvbeckmann@gmail.com>
2017-02-21 17:26:26 -0600

    Fixed several bugs in univariate em implementation

--- clustering/em.py
+++ clustering/em.py
@@ -1,4 +1,5 @@
-import scipy.stats.norm
+from scipy.stats import norm
+import random
 
 def em_univariate(points, distributions):
     """Executes the Expectation / Maximization Algorithm on the data.
@@ -6,34 +7,50 @@ def em_univariate(points, distributions):
     the number of distributions to use in the algorithm."""
 
     # TODO: Init: randomly assign weights to points
+    tol = .001
+    weights = []
+    means = []
+    for x in range(len(points)):
+        temp = [random.random() for i in range(distributions)]
+        s = sum(temp)
+        weight_vec = [i / s for i in temp]
+        weights.append(weight_vec)
 
     while True:
 
         # Update phi vector (normalized sum of weights for distribution)
-        weight_sums = map(sum, zip(*weights))
+        weight_sums = list(map(sum, zip(*weights)))
         phi = [x / len(points) for x in weight_sums]
+        print(len(weight_sums))
+        old_means = means
+        means = []
 
         # Update means
         for j in range(distributions):
             total = 0
             for i in range(len(points)):
                 total += weights[i][j] * points[i]
-            means[j] = total / weight_sums[j]
+            means.append(total / weight_sums[j])
 
-        # TODO: Update parameter matrix
+        parameters = []
+
+        # Update parameter matrix
         for j in range(distributions):
             total = 0
             for i in range(len(points)):
                 total += weights[i][j] * pow(points[i] - means[j], 2)
-            parameters[j] = total / weight_sums[j]
+            parameters.append(total / weight_sums[j])
 
-        # TODO: Break if nothing much has changed
-        if (last_ll - log_likelihood) - last_ll <= tol:
+        # Break if nothing much has changed
+        if (sum(means) - sum(old_means)) < tol:
             break;
 
-        # TODO: Perform E step --> Updating weights based on distributions
+        # Perform E step --> Updating weights based on distributions
         for j in range(distributions):
+            temp_weights = []
             for i in range(len(points)):
-                temp_weights[i] = norm.pdf(points[i], means[j], parameters[j]) * phi[j]
+                temp_weights.append(norm.pdf(points[i], means[j], parameters[j]) * phi[j])
             norm_const = sum(temp_weights)
-            weights = [x / norm_const for x in temp_weights]
+            weights[j] = [x / norm_const for x in temp_weights]
+
+    return (means, weights)
